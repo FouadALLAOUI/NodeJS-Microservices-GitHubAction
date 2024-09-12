7c5dc57ad50d0c981a5124a22ef92c8a1424e063
FouadALLAOUI <fouadallaoui70@gmail.com>
2024-09-12 09:56:57 +0100

    Create Piepeline for each microservice attempt 5

--- gateway/index.js
+++ gateway/index.js
@@ -11,6 +11,9 @@ app.use("/customer", proxy("http://localhost:8001"));
 app.use("/shopping", proxy("http://localhost:8003"));
 app.use("/", proxy("http://localhost:8002")); // products
 
+// This comment is a small change for GitHub Action pipeline
+
+
 app.listen(8000, () => {
   console.log("Gateway is Listening to Port 8000");
 });
